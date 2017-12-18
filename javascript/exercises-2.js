9d7767c8f281e3f66e98684080a0cd1e473ccb14
davidjoshua87 <davidjosh_87@yahoo.com>
2017-12-19 05:04:15 +0700

    Penambahan File Exercises-2.js

---
+++ javascript/exercises-2.js
@@ -0,0 +1,41 @@
+var nama = 'Mikael';
+var peran = ''
+if(nama = 'Mikael') {
+  console.log("Halo Mikael, Pilih peranmu untuk memulai game!");
+}
+
+var peran = 'Ksatria'
+var nama = 'Nina';
+if(nama = 'Nina') {
+  console.log("Selamat datang di Dunia Proxytia, Nina");
+}
+if(peran =='Ksatria'){
+  console.log("Halo Ksatria Nina, kamu dapat menyerang dengan senjatamu!")
+}
+else {
+  console.log("Halo  Nina, kamu bukan Ksatria, tidak dapat menyerang sekarang!")
+}
+
+var nama = 'Danu';
+var peran = 'Tabib';
+if(nama = 'Danu') {
+  console.log("Selamat datang di Dunia Proxytia, Danu");
+}
+if(peran =='Tabib'){
+  console.log("Halo Tabib Danu, kamu akan membantu temanmu yang terluka.")
+}
+else {
+  console.log("Halo Danu, kamu bukan Tabib tidak dapat membantu temanmu yang terluka.")
+}
+
+var nama = 'Zero';
+var peran = 'Penyihir';
+if(nama = 'Zero') {
+  console.log("Selamat datang di Dunia Proxytia, Zero");
+}
+if(peran =='Penyihir'){
+  console.log("Halo Penyihir Zero, ciptakan keajaiban yang membantu kemenanganmu!")
+}
+else {
+  console.log("Halo Zero, kamu bukan Penyihir tidak dapat ciptakan keajaiban yang membantu kemenanganmu!")
+}
