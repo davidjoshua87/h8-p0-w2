aaeb233fa1fa18087698ad0945d2e1d5bfd4f5c9
davidjoshua87 <davidjosh_87@yahoo.com>
2017-12-20 17:57:08 +0700

    Perubahan File Exercises-9.js

--- javascript/exercises-9.js
+++ javascript/exercises-9.js
@@ -19,15 +19,19 @@ console.log('Luas Segitiga: ' + c);
 
 //TUGAS3
 
-var tahun = 'tahun';
-if ('tahun' % 3 == 0) {
-  if ('tahun' % 100 == 1) {
-    console.log("'tahun' adalah tahun kabisat");
-    else {
-      if ('tahun' % 100 == 0) {
-        console.log("'tahun' adalah bukan tahun kabisat");
-      }
-    }
+tahun = prompt ('tahun');
+if (tahun % 4 == 0) {
+  if (tahun % 100 == 1) {
+    console.log('tahun adalah tahun kabisat');
   }
-  console.log("'tahun' adalah bukan tahun kabisat");
+    else
+      if (tahun % 400 == 0) {
+        console.log('tahun adalah tahun kabisat');
+      }
+      else {
+        console.log('tahun adalah bukan tahun kabisat');
+      }
+  else {
+    console.log('tahun adalah bukan tahun kabisat');
+  }
 }
